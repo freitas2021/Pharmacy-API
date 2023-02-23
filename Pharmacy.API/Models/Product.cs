bdacc81bbc00b1bab85e262b0030f3adab14ea93
Freitas2021 <assis.freitas@ymail.com>
2023-02-23 11:24:09 -0300

    Add class Product

---
+++ Pharmacy.API/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Pharmacy.API.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public decimal Price { get; set; }  
+        public string Description { get; set; } 
+
+    }
+}
