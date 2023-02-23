ad5b0f3c46a1124a427a65b346bd0d135c3a9a90
Freitas2021 <assis.freitas@ymail.com>
2023-02-23 10:56:50 -0300

    Add ProductController base

---
+++ Pharmacy.API/Controllers/ProductConttoller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pharmacy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductConttoller : ControllerBase
+    {
+    }
+}
